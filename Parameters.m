7910ba205eaeb5fdb88fd16c14eb49587bd1ec16
dimit <dimit@LAPTOP-Susu.pool.ulb.ac.be>
2024-11-30 19:19:49 +0100

    Parameters

---
+++ Parameters.m
@@ -0,0 +1,42 @@
+%%% EXAM4 - GR2 - Parameters
+
+% Rated power (W)
+P_r = 19000; 
+
+% Rated line voltage (V)
+V_r = 400;
+
+% Rated frenquecy (Hz)
+f_r = 50;
+
+% Stator resistance (Ohm)
+R_s = 0.0629;
+
+% Rotor resistance (Ohm)
+R_r = 0.1091;
+
+% Mutual inductance (H)
+L_m = 0.0408;
+
+R_fe = 213.2; % Ohm
+
+% Stator leakage inductance (mH)
+L_ls = 1.8;
+
+% Rotor leakgae inductance (mH)
+L_lr = 1.8;
+
+% Rated speed (rpm)
+N_r = 1460;
+
+% Moment of inertia (kg.m^2)
+J = 0.2799;
+
+% Friction coef (N.m.sec/r)
+B = 0.0129;
+
+% Pole Number
+P = 4;
+
+% Dumping frequency 
+E = 0.85;
